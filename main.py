e78ae8f9ada75f98373e41942178e7f0e6064921
mckaymac <mckaymac@iu.edu>
2019-08-28 20:38:32 -0400

    Updates  LICENSE, README.md, completes assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print("Hello, my name is Mac McKay.")
+print("My favorite game is Payday 2.")
+print("My main concerns are if I'm going to drown in work from the class and my other classes.")
+print("The thing that I'm most excited for this class is to start making games.")
+print("My stackoverflow.com user number is 11991266.")
+print("My github.com user profile url is: https://github.com/mckaymac. My github.com user name is mckaymac.")
